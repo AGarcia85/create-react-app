6be433db961fff5ef50afe329f7293451a78cfaa
AlexGarcia <afgarcia666@gmail.com>
2019-12-13 15:51:07 -0500

    started UI react project added doggy js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+   constructor(){
+     super()
+     this.state = {
+       doggos: ''
+     }
+   }
+   getPups() {
+     fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(dog => {
+        console.log(dog)
+      })
+   }
+   render() {
+     return (
+      <div>
+        <header>
+          <h1>My API</h1>
+        </header>
+
+        <div>
+          <button />
+          <button />
+          <button />
+          <button />
+          <button />
+          <button />
+        </div>
+
+      </div>
+     );
+   }
 }
 
 export default App;
